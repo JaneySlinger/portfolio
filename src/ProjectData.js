92935155d06f82da9de647547a61f1022bca7942
Janey Slinger <janeyslinger@gmail.com>
2020-06-24 20:41:27 +0100

    Changed colour scheme

--- src/ProjectData.js
+++ src/ProjectData.js
@@ -19,26 +19,6 @@ const data = {
                 "name": "Spotfiy Top Tracks for the Month",
                 "description": "ReactJS website to load the most listened to songs on Spotify for a user in the past 4 months.",
                 "img": "./Images/Spotify.png"
-            },
-            {
-                "name": "Alexa Timetable Skill 2",
-                "description": "An Alexa Skill to load an ical file (designed for University of Nottingham timetables) and provide information specialised to a university timetable format. Developed for my 3rd year dissertation.",
-                "img": "./Images/AlexaTimetable.jpg"
-            },
-            {
-                "name": "Witch Game Unity 2",
-                "description": "A puzzle and item collection game about a witch gathering ingredients for a potion. Made in Unity using C#",
-                "img": "./Images/WitchUnity.PNG"
-            },
-            {
-                "name": "Witch Game Python 2",
-                "description": "A top-down game about a witch finding ingredients for her potion. Uses python arcade library.",
-                "img": "./Images/WitchPython.PNG"
-            },
-            {
-                "name": "Spotfiy Top Tracks for the Month 2",
-                "description": "ReactJS website to load the most listened to songs on Spotify for a user in the past 4 months.",
-                "img": "./Images/Spotify.png"
             }
         ]
 };
