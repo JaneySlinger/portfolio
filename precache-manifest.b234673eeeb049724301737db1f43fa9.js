e1c751cb43fdd87590c84dc0bc11fe1022cc7c9d
Janey Slinger <janeyslinger@gmail.com>
2020-06-25 13:56:46 +0100

    Updates

---
+++ precache-manifest.b234673eeeb049724301737db1f43fa9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ac2cb417cd15441cb2966ddb2d1f0c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "03bb0bbbd93e08aa0248",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d84d4086f7b0728bb1de",
+    "url": "/portfolio/static/css/main.0ba052e4.chunk.css"
+  },
+  {
+    "revision": "03bb0bbbd93e08aa0248",
+    "url": "/portfolio/static/js/2.2e3a98eb.chunk.js"
+  },
+  {
+    "revision": "9a8bb2b00f7c689a2b26d7b5cc824834",
+    "url": "/portfolio/static/js/2.2e3a98eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d84d4086f7b0728bb1de",
+    "url": "/portfolio/static/js/main.fdb8174b.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "0e81101ba0338a9112a310158878a605",
+    "url": "/portfolio/static/media/AlexaTimetable.0e81101b.jpg"
+  },
+  {
+    "revision": "30221af5f1ce6117179c56bdb035ac5b",
+    "url": "/portfolio/static/media/Spotify.30221af5.png"
+  },
+  {
+    "revision": "e931d73edfa1319d26f891ea5176c0db",
+    "url": "/portfolio/static/media/WitchPython.e931d73e.PNG"
+  },
+  {
+    "revision": "7a1ca09bcba8fc194ec3d1d5897c155a",
+    "url": "/portfolio/static/media/WitchUnity.7a1ca09b.PNG"
+  },
+  {
+    "revision": "b7732b4901a941545df10b2cdb0e0574",
+    "url": "/portfolio/static/media/colour scheme.b7732b49.PNG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
