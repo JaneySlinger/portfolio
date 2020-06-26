1e870e696250ce1ec6431068ad20cdedbc717713
Janey Slinger <janeyslinger@gmail.com>
2020-06-26 13:48:09 +0100

    Changed descriptions, added language tag

--- src/Project.js
+++ src/Project.js
@@ -9,14 +9,13 @@ function Project(props) {
             <Card.Img variant="top" src={require("" + props.project.img)}/>
             <Card.Body className="cardContent"> 
               <Card.Title >{props.project.name}</Card.Title>
-              <Card.Subtitle>Python</Card.Subtitle>
+              <Card.Subtitle style={{color: '#123C69'}}>{props.project.language}</Card.Subtitle>
               <Card.Text>
                 {props.project.description}
               </Card.Text>
               <Card.Footer>
               <Button style={{backgroundColor: '#AC3B61', border: '2px solid #AC3B61', color: 'white', borderRadius: '3px', textTransform: 'uppercase'}} href={props.project.url}>View Code</Button>
               </Card.Footer>
-              
             </Card.Body>
           </Card>
     );
--- src/ProjectData.js
+++ src/ProjectData.js
@@ -3,31 +3,35 @@ const data = {
             {
                 "name": "Alexa Timetable Skill",
                 "description": "An Alexa Skill to load an ical file (designed for University of Nottingham timetables) and provide information specialised to a university timetable format. Developed for my 3rd year dissertation.",
-                "img": "./Images/AlexaTimetable.jpg",
-                "url": "https://github.com/JaneySlinger/Alexa-Timetable-Skill-Dissertation-Code"
+                "img": "./Images/AlexaTimetable.PNG",
+                "url": "https://github.com/JaneySlinger/Alexa-Timetable-Skill-Dissertation-Code",
+                "language": "Python"
             },
             {
                 "name": "Witch Game Unity",
-                "description": "A puzzle and item collection game about a witch gathering ingredients for a potion. Made in Unity using C#",
+                "description": "A puzzle and item collection game about a witch gathering ingredients for a potion. Made in Unity using C#. Some models created in blender.",
                 "img": "./Images/WitchUnity.PNG",
-                "url": "https://github.com/JaneySlinger/Witch-Game-Unity"
+                "url": "https://github.com/JaneySlinger/Witch-Game-Unity",
+                "language": "C#"
             },
             {
                 "name": "Witch Game Python",
-                "description": "A top-down game about a witch finding ingredients for her potion. Uses python arcade library.",
+                "description": "A top-down game about a witch finding ingredients for her potion. Made using python arcade library.",
                 "img": "./Images/WitchPython.PNG",
-                "url": "https://github.com/JaneySlinger/Witch_Game_01" 
+                "url": "https://github.com/JaneySlinger/Witch_Game_01",
+                "language": "Python" 
             },
             {
                 "name": "Spotfiy Top Tracks for the Month",
                 "description": "ReactJS website to load the most listened to songs on Spotify for a user in the past 4 months.",
                 "img": "./Images/Spotify.png",
-                "url": "https://github.com/JaneySlinger/spotify-top-month-tracks"
+                "url": "https://github.com/JaneySlinger/spotify-top-month-tracks",
+                "language": "React JS"
             },
             {
                 "name": "Alexa Timetable Skill",
                 "description": "An Alexa Skill to load an ical file (designed for University of Nottingham timetables) and provide information specialised to a university timetable format. Developed for my 3rd year dissertation.",
-                "img": "./Images/AlexaTimetable.jpg",
+                "img": "./Images/AlexaTimetable.PNG",
                 "url": "https://github.com/JaneySlinger/Alexa-Timetable-Skill-Dissertation-Code"
             },
             {
